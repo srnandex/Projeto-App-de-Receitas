7a423b7e1c8456e8c7f840d17581133f0eab23eb
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-04-26 15:39:04 -0300

    provider app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import RecipesProvider from './context/RecipesProvider';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-      </Switch>
-    </BrowserRouter>
+    <RecipesProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </BrowserRouter>
+    </RecipesProvider>
   );
 }
 
