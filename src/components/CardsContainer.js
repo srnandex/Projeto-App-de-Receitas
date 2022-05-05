e16dcc5fbe95bfa9ca2f7ff4ef07ffd249d5e1f5
Andre <andre360.ars@gmail.com>
2022-05-05 16:06:11 -0300

    Fix: (CardsContainer) - Refatora componente para diminuir complexidade

--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -4,64 +4,45 @@ import { Link } from 'react-router-dom';
 import Recipescard from './RecipesCard';
 import style from '../CSS/CardsContainer.module.css';
 
-export default function CardsContainer({ rote, recipes, limit, testId, nameId }) {
+const mealTags = ['strMealThumb', 'strMeal', 'idMeal', 'foods'];
+const drinkTags = ['strDrinkThumb', 'strDrink', 'idDrink', 'drinks'];
+export default function CardsContainer({ rote, recipes, limit, testId }) {
+  let tags = [];
   if (rote === '/foods' || rote.includes('drinks/')) {
-    const id = rote.includes('drinks/')
-      ? '-recomendation-title' : '-card-name';
-    return (
-      <>
-        {recipes.map(({ strMealThumb, strMeal, idMeal }, i) => {
-          if (!strMealThumb && !idMeal) return null;
-          if (i < limit) {
-            let clas = '';
-            if (i > 1 && rote.includes('drinks/')) clas = 'hide';
+    tags = mealTags;
+  } else {
+    tags = drinkTags;
+  }
+  const id = rote.includes('s/')
+    ? '-recomendation-title' : '-card-name';
 
-            return (
-              <Link
-                to={ `/foods/${idMeal}` }
-                key={ strMeal + i }
-                className={ `${style.card} ${clas}` }
-                data-testid={ `${i}${testId}` }
-              >
-                <Recipescard
-                  src={ strMealThumb }
-                  nameId={ `${i}${id}` }
-                  name={ strMeal }
-                  imgId={ `${i}-card-img` }
-                />
-              </Link>);
-          }
-          return null;
-        })}
-      </>);
-  }
-  if (rote === '/drinks' || rote.includes('foods/')) {
-    return (
-      <>
-        {recipes.map(({ strDrinkThumb, strDrink, idDrink }, i) => {
-          if (i < parseFloat(limit)) {
-            let clas = '';
-            if (i > 1 && rote.includes('foods/')) clas = 'hide';
-            return (
-              <Link
-                to={ `/drinks/${idDrink}` }
-                key={ strDrink }
-                className={ `${style.card} ${clas}` }
-                data-testid={ `${i}${testId}` }
-              >
-                <Recipescard
-                  src={ strDrinkThumb }
-                  name={ strDrink }
-                  nameId={ `${i}${nameId}` }
-                  imgId={ `${i}-card-img` }
-                />
-              </Link>);
-          }
-          return null;
-        })}
-      </>);
-  }
+  return (
+    recipes.map((recipe, i) => {
+      if (!recipe[tags[0]] && !recipe[tags[2]]) return null;
+      if (i < limit) {
+        let clas = '';
+        if (i > 1 && rote.includes('s/')) clas = 'hide';
+
+        return (
+          <Link
+            to={ `/${tags[3]}/${recipe[tags[2]]}` }
+            key={ recipe[tags[1]] + i }
+            className={ `${style.card} ${clas}` }
+            data-testid={ `${i}${testId}` }
+          >
+            <Recipescard
+              src={ recipe[tags[0]] }
+              nameId={ `${i}${id}` }
+              name={ recipe[tags[1]] }
+              imgId={ `${i}-card-img` }
+            />
+          </Link>);
+      }
+      return null;
+    })
+  );
 }
+
 CardsContainer.propTypes = {
   rote: PropTypes.string,
   recipes: PropTypes.arrayOf(),
